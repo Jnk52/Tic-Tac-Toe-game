687b87b19a6c9f11577b27b35527f0b28cc20ff5
jnk52 <ajienkyanikam52@gmail.com>
2020-06-13 10:18:28 -0700

    determine winner or tie

--- game.sh
+++ game.sh
@@ -1,49 +1,38 @@
 #!/bin/bash
 
-echo "Welcome to the Tic Tac Toe game"
-#declaring the array for board positions
-flag=1;
-sheet=(1 2 3 4 5 6 7 8 9)
-number=0;
-player=1;
-computer=0;
-#declaring the function for resetting the board
-setTheBoard() {
-        for position in {1..9}
-        do
-                sheet[((number++))]=$position
-        done
-        echo "${sheet[0]} | ${sheet[1]} | ${sheet[2]}"
-        echo "${sheet[3]} | ${sheet[4]} | ${sheet[5]}"
-        echo "${sheet[6]} | ${sheet[7]} | ${sheet[8]}"
-}
-setTheBoard
+flag=1
+declare -a sheet
+player=1
+computer=0
+count=0
+#DECLARING THE ARRAY FOR BOARD POSITIONS
 
+setTheBoard() {
+        echo "${sheet[1]} | ${sheet[2]} | ${sheet[3]}"
+        echo "${sheet[4]} | ${sheet[5]} | ${sheet[6]}"
+        echo "${sheet[7]} | ${sheet[8]} | ${sheet[9]}"
+}
 
 #TOSS BEFORE THE GAME
-
-#lets take a function for toss
-toss(){
-        #lets decalre raandom variable for toss
+#LETS TAKE A FUNCTION FOR A TOSS
+toss() {
+        #LETS DECLARE RANDOM VARIABLE FOR THE TOSS
         tossDecision=$((RANDOM%2))
-
-        #check who wins the toss
         if [ $tossDecision -eq $player ]
         then
-                echo "player won the toss and he is gonna start the game"
+                echo "Player won the toss and he is gonna start the game"
         else
                 echo "computer won the toss and gonna start the game"
         fi
-        }
-toss
+}
 
 #DECLARATION OF LETTERS
+#LETS TAKE A FUNCTION FOR DECLARING LETTERS
 
-#lets take function for declaring letters
-letterSymbols(){
-if [ $tossDecision -eq $player ]
+letterSymbol() {
+        if [[ $tossDecision -eq $player ]]
         then
-                read -p "SELECT LETTER SYMBOL x OR  o TO PLAY THE GAME: " playerSymbol
+                read -p "SELECT THE LETTER SYMBOL x OR o TO PLAY THE GAME: " playerSymbol
                 if [ "$playerSymbol" == "o" ]
                 then
                         computerSymbol="x"
@@ -51,7 +40,7 @@ if [ $tossDecision -eq $player ]
                         computerSymbol="o"
                 fi
         else
-                #COMPUTER TURN RANDOMLY CHOOSES x or o
+                #COMPUTER TURN RANDOMLY CHOOSES x OR o
 
                 computerSymbolToss=$((RANDOM%2))
                 if [ $computerSymbolToss -eq 1 ]
@@ -63,25 +52,135 @@ if [ $tossDecision -eq $player ]
                         playerSymbol="o"
                 fi
         fi
-                }
-letterSymbols
+}
+#LETS CREATE A  FUNCTION TO SEE WHO WINS
+checkWinner() {
+                symbol=$1
+                winner=$2
+                # CHECKING WITHE ROWS
 
-echo "computer symbol= $computerSymbol"
-echo "player symbol= $playerSymbol"
+                if [[ ${sheet[1]} == $symbol && ${sheet[2]} == $symbol && ${sheet[3]} == $symbol ]]
+                then
+                        echo "====$winner is winner====="
+                        exit
+                elif [[ ${sheet[4]} == $symbol && ${sheet[5]} == $symbol && ${sheet[6]} == $symbol ]]
+                then
+                        echo "====$winner is winner====="
+                        exit
+                elif [[ ${sheet[7]} == $symbol && ${sheet[8]} == $symbol && ${sheet[9]} == $symbol ]]
+                then
+                        echo "====$winner is winner====="
+                        exit
+                #CHECKING WITHE COLOUMN
 
-while (( flag >= 1 ))
-do
-        if [ $tossDecision -eq $player ]
+                elif [[ ${sheet[1]} == $symbol && ${sheet[4]} == $symbol && ${sheet[7]} == $symbol ]]
+                then
+                        echo "====$winner is winner====="
+                        exit
+                elif [[ ${sheet[2]} == $symbol && ${sheet[5]} == $symbol && ${sheet[8]} == $symbol ]]
+                then
+                        echo "====$winner is winner====="
+                        exit
+                elif [[ ${sheet[3]} == $symbol && ${sheet[6]} == $symbol && ${sheet[9]} == $symbol ]]
+                then
+                        echo "====$winner is winner====="
+                        exit
+                #CHECKING WITH DIAGONALS
+
+                elif [[ ${sheet[1]} == $symbol && ${sheet[5]} == $symbol && ${sheet[9]} == $symbol ]]
+                then
+                        echo "====$winner is winner====="
+                        exit
+                elif [[ ${sheet[3]} == $symbol && ${sheet[5]} == $symbol && ${sheet[7]} == $symbol ]]
+                then
+                        echo "====$winner is winner====="
+                        exit
+                fi
+}
+
+#CHECK IF IT IS TIE
+Tie() {
+        for (( count = 1; count <= 9; count++ ))
+        do
+                if [ -z "${sheet[count]}" ]
+                then
+                        echo "The game is not tie"
+                        break
+                elif (( $count == 9 ))
+                then
+                        echo "****Game tie****"
+                        exit
+                fi
+        done
+}
+
+#LETS ASSUME FIRST PLAYER STARTS TO PLAY
+playerStarts() {
+        echo "****player starts****"
+        win="player"
+        setTheBoard
+        read -p "Mention the position from where player wants to start the game: " playerPosition
+        if [ -z "${sheet[$playerPosition]}" ]
         then
-                setTheBoard
-                read -p "ENTER POSITION ON SHEET TO START THE GAME WITH: " playerPosition
-                sheet[$playerPosition]=$playerSymbol
-                setTheBoard
+                if (( $playerPosition >= 1 && $playerPosition <= 9 ))
+                then
+                        sheet[$playerPosition]=$playerSymbol
+                        #symbol=$playerSymbol
+                        setTheBoard
+                        checkWinner $playerSymbol $win
+                        Tie
+                else
+                        echo "Not a valid input"
+                        playerStarts
+                fi
         else
-                computerPosition=$((1+RANDOM%9))
+
+                echo "Not a valid input"
+                playerStarts
+        fi
+}
+
+#COMPUTER STARTS TO PLAY
+computerPlay() {
+        echo "****computer plays****"
+        win2="computer"
+        computerPosition=$((RANDOM%8))
+        if [ -z "${sheet[$computerPosition]}" ]
+        then
                 sheet[$computerPosition]=$computerSymbol
                 setTheBoard
+                checkWinner $computerSymbol $win2
+                Tie
+        else
+                computerPlay
+
         fi
-        flag=0
-done
+        echo "****computer played****"
+}
+
+
+#Resume the game
+resumeGame() {
+        if [ $tossDecision -eq $player ]
+        then
+                while [ $flag -eq 1 ]
+                do
+                        playerStarts
+                        computerPlay
+                done
+
+  else
+                while [ $flag -eq 1  ]
+                do
+                        computerPlay
+                        playerStarts
+                done
+        fi
+}
+
+toss
+letterSymbol
+echo "system symbol = $systemSymbol"
+echo "player symbol = $playerSymbol"
+resumeGame
 
