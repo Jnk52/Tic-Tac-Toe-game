d5f195ff10c2a40bdd80ef28b7dcb8ceca8cc415
jnk52 <ajienkyanikam52@gmail.com>
2020-06-13 09:54:17 -0700

    Begin with a toss

--- game.sh
+++ game.sh
@@ -1,17 +1,21 @@
-#!/bin/bash
+#!/bin/bash -x
 
-echo "Welcome to the Tic Tac Toe game"
-#declaring the array for board positions
-sheet=(1 2 3 4 5 6 7 8 9)
-number=0
-#declaring the function for resetting the board
-setTheBoard() {
-        for position in {1..9}
-        do
-                sheet[((number++))]=$position
-        done
-        echo "${sheet[0]} | ${sheet[1]} | ${sheet[2]}"
-        echo "${sheet[3]} | ${sheet[4]} | ${sheet[5]}"
-        echo "${sheet[6]} | ${sheet[7]} | ${sheet[8]}"
-}
-setTheBoard
+player=1;
+computer=0;
+
+#TOSS BEFORE THE GAME
+
+#lets take a function for toss
+toss() {
+        #lets decalre raandom variable for toss
+        tossDecision=$((RANDOM%2))
+
+        #check who wins the toss
+        if [ $tossDecision -eq $player ]
+        then
+                echo "player won the toss and he is gonna start the game"
+        else
+                echo "computer won the toss and gonna start the game"
+        fi
+        }
+toss
